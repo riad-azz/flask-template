9a026646b73f894dd1f87bdefa146e79d026da54
Riadh <riadh.azzoun@hotmail.com>
2023-04-04 18:05:46 +0100

    Update Refactored folders structure Switched to tailwindcss for styling

--- app/__init__.py
+++ app/__init__.py
@@ -9,14 +9,20 @@ from os import path
 from pathlib import Path
 
 # Flask App
-BASE_DIR = Path(__file__).resolve().parent
-STATIC_DIR = path.join(BASE_DIR, 'static')
-app = Flask(__name__, static_folder=STATIC_DIR, static_url_path='')
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_DIR = path.join(BASE_DIR, "static")
+TEMPLATE_DIR = path.join(BASE_DIR, "templates")
+app = Flask(
+    __name__,
+    static_folder=STATIC_DIR,
+    static_url_path="",
+    template_folder=TEMPLATE_DIR,
+)
 
 # App Configs
-app.config['TESTING'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{BASE_DIR}/database/database.db'
-app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
+app.config["TESTING"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{BASE_DIR}/database/database.db"
+app.config["SECRET_KEY"] = "YOUR_SECRET_KEY"
 
 # ---- App Managers ----
 # Database manager
@@ -34,4 +40,3 @@ from app import models
 
 # App Routes
 from app import views
-
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/templates/**/*.html",
+  "./app/templates/*.html",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
